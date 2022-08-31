9e1276a5671ef244a3b0080aa0ccece6ce3ac63d
Bob Reselman <bob@CogArtTech.com>
2022-08-31 13:13:23 -0700

    updating compile bash company

--- sol/compile_byte_code.sh
+++ sol/compile_byte_code.sh
@@ -7,4 +7,9 @@ if [ -d "$BINARY_DIR" ]; then rm -Rf $BINARY_DIR; fi
 
 # compile the file PhoneBookContract.sol to the ./bin directory
 
+echo "Starting the smart contract compilation process."
+
 solcjs --bin ./PhoneBookContract.sol -o $BINARY_DIR
+
+echo "Finished the smart contract compilation process."
+
